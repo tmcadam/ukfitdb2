8c90b90a39bcd97934e49c9deca646aff12effda
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-27 18:28:51 +0900

    Installed pappaparse and loaded resulst table from Google Sheets

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PUBLICATIONS } from '../load-publications';
+//import { PUBLICATIONS } from '../load-publications';
+import * as Papa from "papaparse";
 
 @Component({
   selector: 'app-results',
@@ -8,11 +9,18 @@ import { PUBLICATIONS } from '../load-publications';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  publications;
+  constructor() {
+   }
 
   ngOnInit() {
+      Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6MirpSf_ARZD9wrv3PzSiAjWU7JwbmK64j91p_kUi4uter83dLSdzsrX8NwO4Tu28-aMs6s05dfd6/pub?gid=845632468&single=true&output=csv", {
+      	download: true,
+        header: true,
+      	complete: (function(results) {
+                this.publications = results.data;
+                console.log("Downloading publications finished.");
+      	}).bind(this)
+      });
   }
-
-  publications = PUBLICATIONS;
-
 }
