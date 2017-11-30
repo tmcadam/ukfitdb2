d7b763239981a6b03be705a327634b6946b9057e
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-30 15:09:35 +0900

    Tests for app and introduction components

--- src/app/introduction/introduction.component.spec.ts
+++ src/app/introduction/introduction.component.spec.ts
@@ -1,25 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement }    from '@angular/core';
+import { By }              from '@angular/platform-browser';
 
 import { IntroductionComponent } from './introduction.component';
 
 describe('IntroductionComponent', () => {
-  let component: IntroductionComponent;
-  let fixture: ComponentFixture<IntroductionComponent>;
+    let component:    IntroductionComponent;
+    let fixture:      ComponentFixture<IntroductionComponent>;
+    let el:           HTMLElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ IntroductionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ IntroductionComponent ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
+    beforeEach(() => {
     fixture = TestBed.createComponent(IntroductionComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    el = fixture.debugElement.nativeElement;
+    });
 
-  it('should create', () => {
+    it('should create', () => {
     expect(component).toBeTruthy();
-  });
+    });
+
+    it('should display introduction if state equals home', () => {
+      component.state = 'home';
+      fixture.detectChanges();
+      expect(el.querySelector('h3')).not.toBeNull();
+      expect(el.querySelector('h3').textContent).toContain('Welcome to the UKFIT Publications Database');
+    });
+
+    it('should not display introduction if state DOES NOT equal home', () => {
+      component.state = 'results';
+      fixture.detectChanges();
+      expect(el.querySelector('h3')).toBeNull();
+    });
+
 });
