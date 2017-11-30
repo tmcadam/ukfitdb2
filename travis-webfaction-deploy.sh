95d6683f5855bfb8d97916e003d9390a33141961
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-30 21:16:05 +0900

    First cut of .travis.yml

---
+++ travis-webfaction-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Deploying to Webfaction"
+
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${PROJECT_DIR}/dist/"
+zip -r "${PROJECT_DIR}/dist-${1}.zip" ./ >/dev/null 2>&1
+cd "${PROJECT_DIR}"
+
+if [ ${1} = "staging" ];
+then
+    echo "Deploying to staging server"
+    DEPLOY_PATH=$DEPLOY_PATH_STAGING
+else
+    echo "Deploying to production server"
+fi
+
+export SSHPASS=$DEPLOY_PASS
+sshpass -p ${DEPLOY_PASS} scp "${PROJECT_DIR}/dist-${1}.zip" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}
+sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST rm -rf "${DEPLOY_PATH}/*"
+sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST unzip -q "${DEPLOY_HOME}/dist-${1}.zip" -d "${DEPLOY_PATH}/"
+sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST rm "${DEPLOY_HOME}/dist-${1}.zip"
