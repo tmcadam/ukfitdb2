c1797546230df42816324fa47db540f0ea0d0dd9
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-30 15:11:43 +0900

    Changed search to handle phrases + added unit tests

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -6,15 +6,48 @@ export class SearchService {
     results = [];
     constructor(private publicationsService: PublicationsService) { }
 
-    matchPubs(searchTerm, publication) {
-        var re = new RegExp(searchTerm, 'i');
-        var result: boolean;
-        ( publication.title.search(re) >= 0 ) ? result = true : result = false;
-        return result;
+    splitTerms(_searchTerm) {
+        let words: string[] = [];
+        let matches, re: RegExp;
+        // Look for phrases
+        re = new RegExp("\"([^\"]*)\"", 'g');
+        while ((matches = re.exec(_searchTerm)) != null) {
+            words.push(matches[1]);
+        }
+        // Clean the phrases from _searchTerm
+        words.forEach(function (word) {
+             _searchTerm = _searchTerm.replace(word, "");
+        });
+        // Look for words
+        re = new RegExp('\\w+', 'g');
+        if ((matches = _searchTerm.match(re)) != null) {
+            words = words.concat(matches);
+        }
+        // Clean all the phrases
+        words.map(function(word, i, ar){
+            ar[i] = word.replace(/(^\W+)|(\W+$)/g, "");
+        });
+        return words;
+    }
+
+    matchPubs(searchTerms, publication) {
+        let overallResult: boolean = true;
+        searchTerms.forEach(function (term) {
+            let re = new RegExp('\\b' + term, 'i');
+            let wordResult: boolean = false;
+            if ( publication.title.search(re) >= 0 ) { wordResult = true };
+            if ( publication.keywords.search(re) >= 0 ) { wordResult = true };
+            if ( publication.authors.search(re) >= 0 ) { wordResult = true };
+            if ( publication.year.search(re) >= 0 ) { wordResult = true };
+            if (!wordResult) { overallResult = false};
+        });
+        return overallResult;
     }
 
     search(_searchTerm: string) {
-        this.results = this.publicationsService.publications.filter(this.matchPubs.bind(null, _searchTerm));
+        let searchTerms = this.splitTerms(_searchTerm);
+        this.results = this.publicationsService.publications.filter(this.matchPubs.bind(null, searchTerms));
+
         console.log("Search Term:" + _searchTerm + " Found:" + this.results.length);
     }
 
