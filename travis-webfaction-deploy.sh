720835746275c14486833bdb232d880d29105653
Tom McAdam <tmcadam@users.noreply.github.com>
2017-12-12 10:14:14 +0900

    Updated deploy to push backup scripts to webfaction

--- travis-webfaction-deploy.sh
+++ travis-webfaction-deploy.sh
@@ -3,6 +3,7 @@
 echo "Deploying to Webfaction"
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SSHPASS=$DEPLOY_PASS
 
 cd "${PROJECT_DIR}/dist/"
 zip -r "${PROJECT_DIR}/dist-${1}.zip" ./ >/dev/null 2>&1
@@ -14,9 +15,13 @@ then
     DEPLOY_PATH=$DEPLOY_PATH_STAGING
 else
     echo "Deploying to production server"
+    # Install the backup scripts if in prod
+    sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no "${PROJECT_DIR}/daily-backup.sh" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}/db_backups/ukfitdb2
+    sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no "${PROJECT_DIR}/monthly-backup.sh" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}/db_backups/ukfitdb2
+    sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no "${PROJECT_DIR}/install_cronjobs.sh" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}/db_backups/ukfitdb2
+    sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST bash "${DEPLOY_HOME}/db_backups/ukfitdb2/install_cronjobs.sh"
 fi
 
-export SSHPASS=$DEPLOY_PASS
 sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no "${PROJECT_DIR}/dist-${1}.zip" ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_HOME}
 sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST rm -rf "${DEPLOY_PATH}/*"
 sshpass -e ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST unzip -q "${DEPLOY_HOME}/dist-${1}.zip" -d "${DEPLOY_PATH}/"
